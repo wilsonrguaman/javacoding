902adc1ac3bd77397009888f85646897ebd7a1c0
wilsonrguaman <87439959+wilsonrguaman@users.noreply.github.com>
2021-09-15 18:51:59 -0500

    Update Color18.java

--- javapractice/Color18.java
+++ javapractice/Color18.java
@@ -1,3 +1,11 @@
+/*
+
+Write the method below that takes an integer array myArray. The method returns the value in array myArray that appears the most. For example, if myArray were the array
+below, the method would return the value 39, because 39 appears five times in myArray and no other integer appears that many times.
+You may assume that some value in myArray does appear more frequently than all others. 
+myArray = { 81, 81, 39, 17, 39, 17, 17, 17, 81, 81, 39, 5, 39, 39};
+*/
+
 public class Color18
 {
    public static void main(String[] args)
