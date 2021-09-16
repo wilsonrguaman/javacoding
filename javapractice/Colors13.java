123454e26d6722842f6e0b331b928b2163ae1245
wilsonrguaman <87439959+wilsonrguaman@users.noreply.github.com>
2021-09-15 19:12:16 -0500

    Update Colors13.java

--- javapractice/Colors13.java
+++ javapractice/Colors13.java
@@ -1,3 +1,28 @@
+/*
+Write a program that has the user enter integers between 0 and 9999 unitl the user enters some 
+integers twice. Your program then prints to the console window how many distinct integers were 
+entered. You may assume all integers entered by the user are between 0 and 9999. Here are some
+samples runs:
+--------------------
+Enter an int: 32
+Enter an int: 32
+
+You entered 1 value.
+-------------------------
+Enter an int: 32
+Enter an int: 9285
+Enter an int: 575
+Enter an int: 7153
+Enter an int: 8
+Enter an int: 575
+
+You entered 5 values. 
+-------------------------
+
+*/
+
+
+
 import java.util.Scanner;
    public class Colors13
 {
